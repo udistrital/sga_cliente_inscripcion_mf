ef914542db67d18bdd1bef1605a077a07d5c68cf
diegofceliv <fernando.dfcv@gmail.com>
2024-03-22 00:24:10 -0500

    feat: udistrital/sga_documentacion#125 Se integra spinner-util

--- src/app/interceptors/snipper.interceptor.ts
+++ src/app/interceptors/snipper.interceptor.ts
@@ -1,12 +1,12 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, finalize } from "rxjs";
-import { SpinnerService } from "../services/spinner.service";
+import { SpinnerUtilService } from "spinner-util";
 
 @Injectable()
 export class SpinnerInterceptor implements HttpInterceptor {
 
-    constructor(private spinnerService: SpinnerService) { }
+    constructor(private spinnerService: SpinnerUtilService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         this.spinnerService.show()
