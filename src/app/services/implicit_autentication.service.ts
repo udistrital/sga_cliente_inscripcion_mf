f552cf8b817b6e63d6d430251a0a135dfdc062b2
bfmunozm <swsbmm@gmail.com>
2024-05-26 13:44:43 -0500

    feat: Eliminar console.log udistrital/sga_documentacion#251

--- src/app/services/implicit_autentication.service.ts
+++ src/app/services/implicit_autentication.service.ts
@@ -122,7 +122,7 @@ export class ImplicitAutenticationService {
                     this.clearUrl();
                     localStorage.setItem('user', btoa(JSON.stringify({ ...{ user: payload }, ...{ userService: res } })));
                     this.userSubject.next({ ...{ user: payload }, ...{ userService: res } });
-                }, (error) => (console.log(error))
+                }, (error) => (console.error(error))
                 );
             this.httpOptions = {
                 headers: new HttpHeaders({
@@ -283,7 +283,6 @@ export class ImplicitAutenticationService {
             } else {
                 const timerDelay = expiresIn > this.timeLogoutBefore ? expiresIn - this.timeLogoutBefore : this.timeLogoutBefore;
                 if (!isNaN(expiresIn)) {
-                    console.log(`%cFecha expiración: %c${new Date(expires)}`, 'color: blue', 'color: green');
                     of(null).pipe(delay(timerDelay - this.timeLogoutBefore)).subscribe((data) => {
                         this.logout('logout-auto');
                     });
